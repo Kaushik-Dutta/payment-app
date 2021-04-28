ccc493db33b962d2d3b71fac447962a07b820899
Kaushik Dutta <kaushik-mac@Kaushiks-MacBook-Air.local>
2021-04-28 21:15:46 +0530

    check

---
+++ src/test/java/com/payment/integration_app/IntegrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.integration_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
